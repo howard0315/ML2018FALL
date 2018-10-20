f5af3a8dd8a4dc72347502b089fdfd0e04fa5d6e
howard0315 <r06521504@ntu.edu.tw>
2018-10-20 18:48:22 +0800

    1020 update: initial HW2

---
+++ hw2/hw2.py
@@ -0,0 +1,79 @@
+#-*- coding=utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import sys
+
+class outputData():
+	def __init__(self):
+		pass
+	
+	def DF2CSV(self, estimatedY):
+		pass
+
+class manageData():
+	def __init__(self):
+		pass
+	
+	def scaleXTest(self, method, Xtrain, Xtest):
+		if method == 'minmax':
+			for i in list(Xtrain):
+				minX = Xtrain[i].min()
+				maxX = Xtrain[i].max()
+				Xtest[i] = (Xtest[i] - minX) / (maxX - minX)
+		return Xtest
+	
+	def outputData(self, Ydata):
+		Ydata = calculation().toBinary(Ydata)
+		ID = ['id_' + str(i) for i in range(len(Ydata.Y))]
+		Ydata.insert(loc=0, column='id', value=ID)
+		Ydata.columns = ['id', 'value']
+		Ydata = Ydata.set_index('id')
+		Ydata.to_csv(OutputF)
+		print('done!!')
+
+class calculation():
+	def __init__(self):
+		pass
+	
+	def estimate(self, Xdata, weight, ifB, b):
+		esti = pd.DataFrame(np.zeros((len(Xdata['LIMIT_BAL']), 1)), \
+			columns=['Y'])
+		if ifB:
+			esti['Y'] += b
+		for key in weight:
+			esti['Y'] += weight[key] * Xdata[key]
+		esti = self.sigmoid(esti)
+		return esti
+
+	def sigmoid(self, Ydata):
+		Ydata.Y = 1 + np.exp(-1 * Ydata.Y)
+		Ydata.Y = Ydata.Y.rdiv(1)
+		return Ydata
+	
+	def toBinary(self, Ydata):
+		Ydata.Y[Ydata.Y >= 0.5] = 1
+		Ydata.Y[Ydata.Y < 0.5] = 0
+		Ydata.Y = Ydata.Y.astype(int)
+		return Ydata
+
+if __name__ == '__main__':
+	
+	XTrainF = sys.argv[1]
+	YTrainF = sys.argv[2]
+	XTestF = sys.argv[3]
+	OutputF = sys.argv[4]
+	
+	Spec = np.load('./model.npy')
+	Weight = Spec[0]
+	B = Spec[1]
+	ScalingMethod = Spec[2]
+	
+	Xtrain = pd.read_csv(XTrainF)
+	Ytrain = pd.read_csv(YTrainF)
+	Xtest = pd.read_csv(XTestF)
+	
+	Xtest = manageData().scaleXTest(ScalingMethod, Xtrain, Xtest)
+	
+	Yesti = calculation().estimate(Xtest, Weight, True, B)
+	manageData().outputData(Yesti)
---
+++ hw2/train.py
@@ -0,0 +1,129 @@
+#-*- coding=utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import sys
+
+class logistic():
+	def __init__(self):
+		pass
+	
+	def optimize(self, Ydata, Xdata, scalingMethod, \
+					consideredFeat, LR, N_epoch, ifAddB):
+		weight = self.generateInitialWeight(consideredFeat)
+		b = 0
+		Xdata, dataAdjustment = \
+			manageData().featureScaling(scalingMethod, Xdata, consideredFeat)
+		print('Start training', end='')
+		for i in range(N_epoch):
+			estimatedY = calculation().estimate(Xdata, weight, ifAddB, b)
+			print('\t-lnL: {:f}'.format( \
+				calculation().negativelnL(Ydata, estimatedY)))
+			print(('New epoch: {:>' + str(int(np.log10(N_epoch)) + 1) + \
+					'd}').format((i + 1)), end='')
+			weight, b = self.calcNewWeight(Ydata, estimatedY, Xdata, \
+										weight, LR, ifAddB, b)
+		estimatedY = calculation().estimate(Xdata, weight, ifAddB, b)
+		print('\t-lnL: {:f}'.format( \
+				calculation().negativelnL(Ydata, estimatedY)))
+		print('End of training')
+		print('\nResulting weights:')
+		print('\n{:>12s}\t{:s}'.format('Feature', 'Weight'))
+		print('{:>12s}\t{:s}'.format('-------', '------'))
+		if ifAddB:
+			print('{:>12s}\t{:=9.6f}'.format('CONST', b))
+		for k in consideredFeat:
+			print('{:>12s}\t{:=9.6f}'.format(k, weight[k]))
+		return weight, b, dataAdjustment
+	
+	def generateInitialWeight(self, featName):
+		weight = {}
+		for e in featName:
+			weight[e] = 0.1
+		return weight
+	
+	def calcNewWeight(self, Ydata, estiY, Xdata, weight, LR, ifB, b):
+		estiDiff = pd.DataFrame(np.zeros((len(Ydata['Y']), 1)), \
+			columns=['Y'])
+		estiDiff['Y'] = Ydata['Y'] - estiY['Y']
+		if ifB:
+			b -= LR * (-1 * estiDiff['Y']).sum()
+		for i in weight:
+			weight[i] -= LR * (-1 * (estiDiff['Y'] * Xdata[i])).sum()
+		return weight, b
+
+class manageData():
+	def __init__(self):
+		pass
+
+	def importTrain(self, trainXDir, trainYDir):
+		Xtrain = pd.read_csv(trainXDir)
+		Ytrain = pd.read_csv(trainYDir)
+		return Xtrain, Ytrain
+	
+	def splitData(self, Xdata, Ydata, factor):
+		pass
+	
+	def featureScaling(self, method, Xdata, featureName):
+		if method == 'minmax':
+			scalingSpec = \
+				pd.DataFrame(index=featureName, columns=['min', 'max'])
+			for ft in featureName:
+				minX = Xdata[ft].min()
+				maxX = Xdata[ft].max()
+				Xdata[ft] = (Xdata[ft] - minX) / (maxX - minX)
+				scalingSpec['min'][ft] = minX
+				scalingSpec['max'][ft] = maxX
+		return Xdata, scalingSpec
+
+class calculation():
+	def __init__(self):
+		pass
+	
+	def estimate(self, Xdata, weight, ifB, b):
+		esti = pd.DataFrame(np.zeros((len(Xdata['LIMIT_BAL']), 1)), \
+			columns=['Y'])
+		if ifB:
+			esti['Y'] += b
+		for key in weight:
+			esti['Y'] += weight[key] * Xdata[key]
+		esti = self.sigmoid(esti)
+		return esti
+
+	def sigmoid(self, Ydata):
+		Ydata.Y = 1 + np.exp(-1 * Ydata.Y)
+		Ydata.Y = Ydata.Y.rdiv(1)
+		return Ydata
+	
+	def negativelnL(self, Ydata, estiY):
+		estilnL = pd.DataFrame(np.zeros((len(Ydata.Y), 1)), columns=['-lnL'])
+		estilnL['-lnL'] = \
+			-(Ydata.Y * np.log(estiY.Y) + (1 - Ydata.Y) * np.log(1 - estiY.Y))
+		return estilnL['-lnL'].sum()
+
+	def avgError(self, Ydata, estimatedY, weight):
+		err = Ydata['Y'] - estimatedY['Y']
+		absErr = list(map(abs, err['Y'].tolist()))
+		return sum(absErr) / len(absErr)
+
+if __name__ == '__main__':
+	
+	XDataF = sys.argv[1]
+	YDataF = sys.argv[2]
+	ModelDir = sys.argv[3]
+	
+	XD, YD = manageData().importTrain(XDataF, YDataF)
+	scalingMethod = 'minmax'
+	consideredFeat = list(XD)
+	#consideredFeat = ['LIMIT_BAL', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_0']
+	LR = 1e-4 * 2
+	NumEpoch = 5000
+	AddConst = True
+	
+	finalWeight, finalB, scalingSpec = logistic().optimize(YD, XD, \
+		scalingMethod, consideredFeat, LR, NumEpoch, AddConst)
+	
+	estimatedY = calculation().estimate(XD, finalWeight, AddConst, finalB)
+	nlnL = calculation().negativelnL(YD, estimatedY)
+	
+	np.save(ModelDir, [finalWeight, finalB, 'minmax'])
